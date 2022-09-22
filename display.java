87b64327f0f897443afcead00cea364f2dc620a0
seshupriya08 <seshupriya0821@gmail.com>
2022-09-22 19:08:53 +0530

    created a new file display.java and also modified sum.java

---
+++ display.java
@@ -0,0 +1,8 @@
+class display
+{
+	public static void main(string args[])
+	{
+		system.out.println("hello new life");
+	}
+}
+
--- sum.java
+++ sum.java
@@ -8,6 +8,6 @@ class sum
 		res=a+b;
 		system.out.println("sum of 10 & 25 is :"+res);
 		system.out.println("Thank you & have a nice day");
-		
+		system.out.println("good days are coming as a future devops engineer");
 	}
 }
