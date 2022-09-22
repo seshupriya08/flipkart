f240080ce65de7b93d12841935e8f80f6fe4a3d1
seshupriya08 <seshupriya0821@gmail.com>
2022-09-22 11:34:38 +0530

    created a java file to find the sum of two numbers

--- sum.java
+++ sum.java
@@ -2,12 +2,10 @@ class sum
 {
 	public static void main(string args[])
 	{
-		int a,b,res;
+		int a.b.res;
 		a=10;
 		b=25;
 		res=a+b;
-		system.out.println("sum of 10 & 25is :"+res);
+		system.out.println("sum of 10 & 25 is :"+res);
 	}
-
 }
-
