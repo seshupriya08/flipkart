1816aff21b1ce74fba83b259a8bb40b434f48bae
seshupriya08 <seshupriya0821@gmail.com>
2022-09-23 13:19:51 +0530

    Renamed sum.java as addition.java

---
+++ addition.java
@@ -0,0 +1,13 @@
+class sum
+{
+	public static void main(string args[])
+	{
+		int a.b.res;
+		a=10;
+		b=25;
+		res=a+b;
+		system.out.println("sum of 10 & 25 is :"+res);
+		system.out.println("Thank you & have a nice day");
+		system.out.println("good days are coming as a future devops engineer");
+	}
+}
